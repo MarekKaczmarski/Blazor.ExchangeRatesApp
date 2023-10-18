42625fb80b1a8bab53d6648662f64f67ba12acd5
mkmarco <marconayt@gmail.com>
2023-10-18 22:17:46 +0200

    Add Id property and relations between CurrencyRates and Rate tables

--- ExchangeRatesApp.API/Entities/CurrencyRates.cs
+++ ExchangeRatesApp.API/Entities/CurrencyRates.cs
@@ -2,9 +2,10 @@
 {
     public class CurrencyRates
     {
-            public string Table { get; set; } = string.Empty;
-            public string No { get; set; } = string.Empty;
-            public string EffectiveDate { get; set; } = string.Empty;
-            public List<Rate> Rates { get; set; } = new List<Rate>();
+        public int Id { get; set; }
+        public string Table { get; set; } = string.Empty;
+        public string No { get; set; } = string.Empty;
+        public string EffectiveDate { get; set; } = string.Empty;
+        public List<Rate> Rates { get; set; } = new List<Rate>();
     }
 }
