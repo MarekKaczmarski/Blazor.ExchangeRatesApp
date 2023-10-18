3badf707f5452d9d4d372448e6df02e8ccfd003b
mkmarco <marconayt@gmail.com>
2023-10-18 22:08:13 +0200

    Add Init migrations. Create CurrencyRates & Rates tables and make relationship between them.

---
+++ ExchangeRatesApp.API/Data/ExchangeRatesAppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ExchangeRatesApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ExchangeRatesApp.API.Data
+{
+    public class ExchangeRatesAppDbContext : DbContext
+    {
+        public ExchangeRatesAppDbContext(DbContextOptions<ExchangeRatesAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CurrencyRates> CurrencyRates { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CurrencyRates>()
+                .HasMany(cr => cr.Rates)
+                .WithOne(r => r.CurrencyRates)
+                .HasForeignKey(r => r.CurrencyRatesId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
