9987130e8033ff44cadb7c76255bc93fe633114d
mkmarco <marconayt@gmail.com>
2023-10-26 17:40:13 +0200

    CurrencyRates + Rate models

---
+++ ExchangeRatesApp.Shared/CurrencyRates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesApp.Models
+{
+    public class CurrencyRates
+    {
+        public int Id { get; set; }
+        public string Table { get; set; } = string.Empty;
+        public string No { get; set; } = string.Empty;
+        public string EffectiveDate { get; set; } = string.Empty;
+        public List<Rate> Rates { get; set; } = new List<Rate>();
+    }
+}
---
+++ ExchangeRatesApp.Shared/Rate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesApp.Models
+{
+    public class Rate
+    {
+        public int Id { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public double Mid { get; set; }
+        public int CurrencyRatesId { get; set; }
+        public CurrencyRates? CurrencyRates { get; set; }
+    }
+}
