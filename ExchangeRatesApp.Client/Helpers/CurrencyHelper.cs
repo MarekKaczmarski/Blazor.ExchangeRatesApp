99647998072a6435c6569873dbedb85de252f61c
mkmarco <marconayt@gmail.com>
2023-12-15 02:28:07 +0100

    Move GetFlagImageName to helper class

---
+++ ExchangeRatesApp.Client/Helpers/CurrencyHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ExchangeRatesApp.Client.Helpers
+{
+    public static class CurrencyHelper
+    {
+        public static string GetFlagImageName(string currencyCode)
+        {
+            return $"{currencyCode.ToLower()}.png";
+        }
+    }
+}
