568f79bc694c4c3c4bdd8229c188352dacbc1513
mkmarco <marconayt@gmail.com>
2023-12-15 02:33:07 +0100

    Add CurrencyYTD model

---
+++ ExchangeRatesApp.Shared/RatesChooseDate/CurrencyYTD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesApp.Models.RatesChooseDate
+{
+    public class CurrencyYTD
+    {
+        public string CurrencyCode { get; set; }
+        public decimal YTD { get; set; }
+    }
+}
