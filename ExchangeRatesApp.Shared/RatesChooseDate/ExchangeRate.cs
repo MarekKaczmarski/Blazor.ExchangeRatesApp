a98844b5453471888f37ed038838ab1900e1ee07
mkmarco <marconayt@gmail.com>
2023-12-15 02:34:26 +0100

    Modify EffectiveDate format from string to DateTime

--- ExchangeRatesApp.Shared/RatesChooseDate/ExchangeRate.cs
+++ ExchangeRatesApp.Shared/RatesChooseDate/ExchangeRate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace ExchangeRatesApp.Models.RatesChooseDate
     public class ExchangeRate
     {
         public string No { get; set; }
-        public string EffectiveDate { get; set; }
+
+        [Display(Name = "EffectiveDate")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EffectiveDate { get; set; }
         public decimal Mid { get; set; }
     }
 }
