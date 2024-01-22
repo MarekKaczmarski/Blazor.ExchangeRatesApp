5d673d2d955a0c4760f7279ad01feddb582e64e3
mkmarco <marconayt@gmail.com>
2024-01-22 15:57:49 +0100

    Remove injecting DI from CurrencyDataService

--- ExchangeRatesApp.Client/Services/ICurrencyService.cs
+++ ExchangeRatesApp.Client/Services/ICurrencyService.cs
@@ -8,7 +8,13 @@ namespace ExchangeRatesApp.Client.Services
         Task<List<Rate>> GetAllRatesFromAllTables();
         Task<List<CurrencyRates>> GetAllCurrenciesFromAllTables();
         Task<List<CurrencyRates>> GetAllCurrencies(string table);
+        Task<ExchangeRatesSeries> GetLastXCurrencies(string code, int topCount);
+        Task<ExchangeRatesSeries> TryGetRatesFromTable(HttpClient httpClient, string table, string code, int topCount);
+        Task<ExchangeRatesSeries> GetExchangeRatesOnDate(string code, DateTime date);
+        Task<ExchangeRatesSeries> GetExchangeRatesInRange(string code, DateTime startDate, DateTime endDate);
+        Task<string> TryGetRatesFromTable(string code, HttpClient httpClient, string table);
         Task<Rate?> GetCurrencyByCode(string code);
         Rate? FindCurrencyByCode(IEnumerable<CurrencyRates> currencies, string code);
+        void HandleApiError();
     }
 }
