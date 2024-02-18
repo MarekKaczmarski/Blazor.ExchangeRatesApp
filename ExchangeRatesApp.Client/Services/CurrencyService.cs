b3d9d3e57875490348c8386d01a975951f3eef1b
mkmarco <marconayt@gmail.com>
2024-02-18 01:14:00 +0100

    Modify DI CurrencyRepository -> ICurrencyRepository

--- ExchangeRatesApp.Client/Services/CurrencyService.cs
+++ ExchangeRatesApp.Client/Services/CurrencyService.cs
@@ -7,9 +7,9 @@ namespace ExchangeRatesApp.Client.Services
 {
     public class CurrencyService : ICurrencyService
     {
-        private readonly CurrencyRepository _currencyRepository;
+        private readonly ICurrencyRepository _currencyRepository;
 
-        public CurrencyService(CurrencyRepository currencyRepository)
+        public CurrencyService(ICurrencyRepository currencyRepository)
         {
             _currencyRepository = currencyRepository;
         }
